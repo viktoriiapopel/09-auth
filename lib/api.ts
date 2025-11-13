9cc4513894b82f8e5e09ba8ff5a7e1e7c13cacc0
Viktoriia Popel <xructa2009@gmail.com>
2025-11-13 16:01:11 +0100

    fix task

--- lib/api.ts
+++ lib/api.ts
@@ -1,16 +1,6 @@
 import { Note } from "@/types/note";
 import axios from "axios";
 
-// Видаляємо стару логіку baseURL
-// axios.defaults.baseURL = 'http://localhost:3000/api'
-// const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
-
-// // Створюємо інстанс axios
-// export const api = axios.create({
-//   baseURL,
-//   withCredentials: true,
-// });
-
 export interface FetchNotesParams {
   page?: number;
   perPage?: number;
